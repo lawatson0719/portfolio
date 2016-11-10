778aa419bce9da6f28900d898e16c16d7ce5d146
Lauren Watson <lawatson0719@gmail.com>
2016-11-10 11:50:41 -0500

    more project stuff

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
+var lowdb = require('lowdb');
+var fileAsync = require('lowdb/lib/file-async');
+var shortid = require('shortid');
+var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
 var port = 3000;
+
+var db = lowdb('db.json', { storage: fileAsync });
+
+app.use(bodyParser());
 
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/dist'));
 app.use(express.static(__dirname + '/lib'));
 app.use(express.static(__dirname + '/src/css'));
+
+db.defaults({
+	// any default collections (Arrays)
+}).value();
 
 app.listen(port);
